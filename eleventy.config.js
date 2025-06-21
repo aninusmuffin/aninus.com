95f93e9f359ba3975544dfb2197733ba974084ce
Aninus Partikler <muffin@aninus.com>
2025-06-21 17:33:44 +0200

    Add links to the index page with socials-button components

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ import fontAwesomePlugin from "@11ty/font-awesome";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(fontAwesomePlugin);
+  eleventyConfig.addBundle("customicons");
   eleventyConfig.addPassthroughCopy("_src/styles/*.css");
   eleventyConfig.addPassthroughCopy("_src/img/*");
   eleventyConfig.addPassthroughCopy("_src/CNAME");
