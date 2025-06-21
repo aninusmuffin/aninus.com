47f1ddce1b6d1f3311e68b3132572a776107fa73
Aninus Partikler <muffin@aninus.com>
2025-06-21 16:07:09 +0200

    Switch to @11ty/font-awsome

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
+import fontAwesomePlugin from "@11ty/font-awesome";
+
 export default async function (eleventyConfig) {
+  eleventyConfig.addPlugin(fontAwesomePlugin);
   eleventyConfig.addPassthroughCopy("_src/styles/*.css");
   eleventyConfig.addPassthroughCopy("_src/assets/*");
   eleventyConfig.addPassthroughCopy("_src/CNAME");
