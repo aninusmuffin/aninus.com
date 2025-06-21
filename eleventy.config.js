330e73dd7ee1fb1b7d094af19b0b553560d89f00
Aninus Partikler <muffin@aninus.com>
2025-06-21 17:27:28 +0200

    Make the background image a local file

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,7 @@ import fontAwesomePlugin from "@11ty/font-awesome";
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(fontAwesomePlugin);
   eleventyConfig.addPassthroughCopy("_src/styles/*.css");
-  eleventyConfig.addPassthroughCopy("_src/assets/*");
+  eleventyConfig.addPassthroughCopy("_src/img/*");
   eleventyConfig.addPassthroughCopy("_src/CNAME");
   eleventyConfig.addWatchTarget("_src/styles/**/*.css");
   return { config };
