7ac6e7a657330ad6112db96298f653f057431b8d
Aninus Partikler <muffin@aninus.com>
2025-06-22 09:35:03 +0200

    Move fonts, styles and images to the assets directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,10 +3,9 @@ import fontAwesomePlugin from "@11ty/font-awesome";
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(fontAwesomePlugin);
   eleventyConfig.addBundle("customicons");
-  eleventyConfig.addPassthroughCopy("_src/styles/*.css");
-  eleventyConfig.addPassthroughCopy("_src/img/*");
+  eleventyConfig.addPassthroughCopy("_src/assets/**/*");
   eleventyConfig.addPassthroughCopy("_src/CNAME");
-  eleventyConfig.addWatchTarget("_src/styles/**/*.css");
+  eleventyConfig.addWatchTarget("_src/assets/styles/**/*.css");
   return { config };
 }
 
