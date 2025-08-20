28fa891c4b89cfbce4637a045d3fa9a7f62f0bb7
Aninus Partikler <muffin@aninus.com>
2025-08-20 13:22:39 +0200

    Custom info boxes using markdown-it-obsidian-callouts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,14 @@
 import fontAwesomePlugin from "@11ty/font-awesome";
+import MarkdownItObsidianCallouts from 'markdown-it-obsidian-callouts'
+
 
 export default async function (eleventyConfig) {
+  let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+  
   eleventyConfig.addPlugin(fontAwesomePlugin);
   eleventyConfig.addBundle("customicons");
   eleventyConfig.addPassthroughCopy("_src/assets/styles/base.css")
@@ -8,8 +16,11 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_src/assets/fonts/**/*")
   eleventyConfig.addPassthroughCopy("_src/assets/img/**/*")
   eleventyConfig.addWatchTarget("_src/assets/styles/**/*.css");
+  eleventyConfig.amendLibrary("md",MarkdownItObsidianCallouts);
   return { config };
 }
+
+
 
 export const config = {
   dir: {
