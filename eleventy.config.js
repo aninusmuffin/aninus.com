71a7e1b885d86d664379610f566b42f6efba9c99
Aninus Partikler <muffin@aninus.com>
2025-08-20 13:10:14 +0200

    Move CSS into different files

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,10 @@ import fontAwesomePlugin from "@11ty/font-awesome";
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(fontAwesomePlugin);
   eleventyConfig.addBundle("customicons");
-  eleventyConfig.addPassthroughCopy("_src/assets/**/*");
+  eleventyConfig.addPassthroughCopy("_src/assets/styles/base.css")
+  eleventyConfig.addPassthroughCopy("_src/assets/fonts/**/*")
+  eleventyConfig.addPassthroughCopy("_src/assets/fonts/**/*")
+  eleventyConfig.addPassthroughCopy("_src/assets/img/**/*")
   eleventyConfig.addWatchTarget("_src/assets/styles/**/*.css");
   return { config };
 }
